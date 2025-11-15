4516234c79752936e6dc35fa613179dc756586d3
Minh Ngoc <luongminhngoc29@gmail.com>
2025-11-15 20:41:27 +0700

    Update homepage copies

--- app/page.js
+++ app/page.js
@@ -1,51 +1,56 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Welcome to my personal website! My name is Minh Ngoc
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            I&#39;m an MBA graduate, and also a proud mom of a toddler. I&#39;m restarting my career, targeting Junior Business Analyst role.
-          </p>
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <main
+                className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+                <Image
+                    className="dark:invert"
+                    src="/next.svg"
+                    alt="Next.js logo"
+                    width={100}
+                    height={20}
+                    priority
+                />
+                <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+                    <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+                        Hi there, I’m Minh Ngoc.
+                    </h1>
+                    <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+                        An MBA graduate in Banking and Finance and a proud mom of a toddler, I’m preparing to restart my
+                        career with a focus on IT Business Analyst or Product Owner roles. To achieve this, I’m building
+                        core Business Analysis skills such as requirement elicitation, BRD, BPMN, use cases, user
+                        stories, UML, and Agile methodologies, along with foundational knowledge in UX design and
+                        technical skills including SQL, JavaScript, and Python.
+                    </p>
+                </div>
+                <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+                    <a
+                        className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+                        href="https://resume-virid-zeta.vercel.app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Image
+                            className="dark:invert"
+                            src="/vercel.svg"
+                            alt="Vercel logomark"
+                            width={16}
+                            height={16}
+                        />
+                        My resume
+                    </a>
+                    <a
+                        className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        My Projects
+                    </a>
+                </div>
+            </main>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Hire me
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            My Projects
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
