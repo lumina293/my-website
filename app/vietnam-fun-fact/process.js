8dc811cc3ef355f67ce694e38e098c801b716c82
Minh Ngoc <luongminhngoc29@gmail.com>
2025-11-22 17:44:27 +0700

    Add function for finding biggest and smallest provinces by area

--- app/vietnam-fun-fact/process.js
+++ app/vietnam-fun-fact/process.js
@@ -28,10 +28,34 @@ function countCommunes(provinces) {
     return totalCommunes
 }
 
+function findBiggestProvinceByArea(provinces) {
+    let biggestProvince = provinces[0]
+    for (let i = 1; i < provinces.length; i++) {
+        if (provinces[i].areaInKm2 > biggestProvince.areaInKm2) {
+            biggestProvince = provinces[i];
+        }
+    }
+    return biggestProvince;
+}
+
+function findSmallestProvinceByArea(provinces) {
+    let smallestProvince = provinces[0]
+    for (let i = 1; i < provinces.length; i++) {
+        if (provinces[i].areaInKm2 < smallestProvince.areaInKm2) {
+            smallestProvince = provinces[i];
+        }
+    }
+    return smallestProvince;
+}
+
 function main() {
     let numberOfProvinces = countProvinces(ProvincesData)
     let numberOfCommunes = countCommunes(ProvincesData)
     console.log("Total number of provinces: " + numberOfProvinces + " with " + numberOfCommunes + " communes.")
+    let biggestProvinceByArea = findBiggestProvinceByArea(ProvincesData)
+    console.log("The biggest province is " + biggestProvinceByArea.name + " with area of " + biggestProvinceByArea.areaInKm2 + " km2.")
+    let smallestProvinceByArea = findSmallestProvinceByArea(ProvincesData)
+    console.log("The smallest province is " + smallestProvinceByArea.name + " with area of " + smallestProvinceByArea.areaInKm2 + " km2.")
 }
 
 main()
