013e66618396eb7e46e228238c4ec90ff9123394
Minh Ngoc <luongminhngoc29@gmail.com>
2025-11-24 20:27:47 +0700

    Create vietnam-geo and finance-dashboard page.js files

---
+++ app/finance-dashboard/page.js
@@ -0,0 +1,7 @@
+export default function VietnamGeo() {
+    return (
+        <div>
+            <h1>My Financial Dashboard</h1>
+        </div>
+    )
+}
---
+++ app/vietnam-geo/page.js
@@ -0,0 +1,7 @@
+export default function VietnamGeo() {
+    return (
+        <div>
+            <h1>Hello, I&#39;m going to code here</h1>
+        </div>
+    )
+}
