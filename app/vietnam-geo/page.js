8d83ed6fba00cbd04416a5408f0aab1f989e32c0
Minh Ngoc <luongminhngoc29@gmail.com>
2025-11-25 07:29:12 +0700

    Add header to vietnam-geo page

--- app/vietnam-geo/page.js
+++ app/vietnam-geo/page.js
@@ -1,3 +1,4 @@
+import VietnamGeoHeader from '@/components/VietnamGeoHeader';
 import Provinces from "@/app/vietnam-geo/data";
 import {
     countProvinces,
@@ -24,22 +25,25 @@ export default function VietnamGeo() {
     let lowestDensityProvince = findProvinceWithLowestDensity(Provinces)
 
     return (
-        <div>
-            <h1>Địa lý Việt Nam</h1>
-            <ProvinceCount/>
-            <CommuneCount/>
+        <main>
+            <VietnamGeoHeader/>
+            <div>
+                <h1>Địa lý Việt Nam</h1>
+                <ProvinceCount/>
+                <CommuneCount/>
 
-            <p>Tỉnh {biggestProvince.name} có diện tích lớn nhất với diện tích: {biggestProvince.areaInKm2}</p>
-            <p>Tỉnh {smallestProvince.name} có diện tích nhỏ nhất với diện tích: {smallestProvince.areaInKm2}</p>
+                <p>Tỉnh {biggestProvince.name} có diện tích lớn nhất với diện tích: {biggestProvince.areaInKm2}</p>
+                <p>Tỉnh {smallestProvince.name} có diện tích nhỏ nhất với diện tích: {smallestProvince.areaInKm2}</p>
 
-            <p>Tỉnh đông dân nhất: {mostPopulatedProvince.name} với dân số: {mostPopulatedProvince.population}</p>
-            <p>Tỉnh ít dân nhất: {leastPopulatedProvince.name} với dân số: {leastPopulatedProvince.population}</p>
+                <p>Tỉnh đông dân nhất: {mostPopulatedProvince.name} với dân số: {mostPopulatedProvince.population}</p>
+                <p>Tỉnh ít dân nhất: {leastPopulatedProvince.name} với dân số: {leastPopulatedProvince.population}</p>
 
-            <p>Tỉnh có mật độ dân số lớn nhất: {highestDensityProvince.name} với mật
-                độ: {highestDensityProvince.population / highestDensityProvince.areaInKm2} người/km2</p>
-            <p>Tỉnh có mật độ dân số nhỏ nhất: {lowestDensityProvince.name} với mật
-                độ: {lowestDensityProvince.population / lowestDensityProvince.areaInKm2} người/km2</p>
-        </div>
+                <p>Tỉnh có mật độ dân số lớn nhất: {highestDensityProvince.name} với mật
+                    độ: {highestDensityProvince.population / highestDensityProvince.areaInKm2} người/km2</p>
+                <p>Tỉnh có mật độ dân số nhỏ nhất: {lowestDensityProvince.name} với mật
+                    độ: {lowestDensityProvince.population / lowestDensityProvince.areaInKm2} người/km2</p>
+            </div>
+        </main>
     )
 }
 
