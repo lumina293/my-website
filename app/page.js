22830efbc0ad80618bdfd80f6bfaf438676e1564
Minh Ngoc <luongminhngoc29@gmail.com>
2025-11-27 20:44:45 +0700

    Add My Finance button on home page

--- app/page.js
+++ app/page.js
@@ -48,6 +48,13 @@ export default function Home() {
                     >
                         Vietnam Geo
                     </a>
+                    <a
+                        className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+                        href="/finance-dashboard"
+                        rel="noopener noreferrer"
+                    >
+                        My Finance
+                    </a>
                 </div>
             </main>
         </div>
